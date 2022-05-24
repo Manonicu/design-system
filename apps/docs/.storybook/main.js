f7a553994ee355c31722ba6f09ace9a0f13e7ec1
pfan <gemini0525@foxmail.com>
2022-05-24 11:03:47 +0800

    chore: refactor name

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "manon-core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/manon-core/"
             ),
           },
         ],
